ff9ac0f55ac58d0bf4c074b3e4566501efd1be5e
Toshiki Fujiyoshi <toshichan@synapz.jp>
2018-04-27 22:24:24 +0900

    ユーザCRUD

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.attributes = user_params
+    if @user.save!
+      redirect_to user_url(@user)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :email,
+                                 :password, :password_confirmation)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
   has_many :pictures
   has_many :favorites
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :name,  presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+            uniqueness: true
+
+  before_save { email.downcase! }
 end
