b56f6130e6e943b66f5f6e814b7393a329f101ac
Toshiki Fujiyoshi <toshichan@synapz.jp>
2018-04-27 23:47:30 +0900

    ログイン機能追加

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,14 @@
+module SessionsHelper
+  
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  def logged_in?
+    current_user.present?
+  end
+  
+  def redirect_non_login_user
+    redirect_to new_session_url unless logged_in?
+  end
+end
