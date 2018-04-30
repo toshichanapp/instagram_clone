9c63edd578e0e2644040d5248ee66c4a758b2c66
Toshiki Fujiyoshi <toshichan@synapz.jp>
2018-04-30 18:54:58 +0900

    picture投稿機能

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,6 @@
 class Picture < ApplicationRecord
+  
+  mount_uploader :image, ImageUploader
   belongs_to :user
   has_many :favorites
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   resources :sessions, only: %i[new create destroy]
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
 end
