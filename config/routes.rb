772d05dd9ce83245ff833db9d6e2aceb8460f081
toshichanapp <toshichan@synapz.jp>
2018-05-03 21:46:49 +0900

    ルートパスの時users/newを表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  resources :users
+  root to: 'users#new'
+  resources :users, except: [:new]
   resources :sessions, only: %i[new create destroy]
   resources :pictures do
     collection do
