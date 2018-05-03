63c107f57a682e085428c9be46f5d983ed00b059
toshichanapp <toshichan@synapz.jp>
2018-05-03 21:47:44 +0900

    user削除時，rootにリダイレクトする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id)
+      redirect_to new_session_path
     else
       render 'new'
     end
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
-    redirect_to users_url
+    redirect_to root_url
   end
   
   private
