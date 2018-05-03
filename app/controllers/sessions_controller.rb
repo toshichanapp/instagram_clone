808fd3b3bec472884c45bfc5ecdd11b41077e5e2
toshichanapp <toshichan@synapz.jp>
2018-05-03 21:10:40 +0900

    ログインエラー処理，デザイン追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,16 @@
 class SessionsController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
   
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
-      redirect_to user_path(user.id), flash: {success: 'ログインに成功しました'}
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id), flash: {success: 'ログインに成功しました'}
     else
-      flash.now[:danger] = 'ログインに失敗しました'
-      render 'new'
+      flash.now[:danger] =
+      redirect_to new_session_url, alert: 'ログインに失敗しました'
     end
   end
   
