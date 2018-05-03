85c1a5091a47f13bcbbfa6db5a75720bbf027e0c
toshichanapp <toshichan@synapz.jp>
2018-05-03 20:02:22 +0900

    precompile

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
   resources :users
   resources :sessions, only: %i[new create destroy]
   resources :pictures do
