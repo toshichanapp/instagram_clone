58bbb877374dbad7acca612dd16b263d3160d009
toshichanapp <toshichan@synapz.jp>
2018-05-04 09:07:16 +0900

    お気に入り一覧の作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,9 @@
 class FavoritesController < ApplicationController
+  
+  def index
+    @favorites = current_user.favorites
+  end
+  
   def create
     favorite = current_user.favorites.create(picture_id: params[:picture_id])
     redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんのブログをお気に入り登録しました"
